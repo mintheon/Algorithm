6e8fe1b34f959b464812135563521345673f17cc
mintheon <mintheon@mintheon.com>
2021-06-13 20:42:38 +0900

    [BOJ, 9466] 텀프로젝트 - 다시풀어볼것

--- src/baekjoon/P09466.java
+++ src/baekjoon/P09466.java
@@ -5,14 +5,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//TODO: 다시 풀어볼것
+
 /**
- * @see <a href="boj.kr/1001">A-B</a>
+ * @see <a href="boj.kr/9466">텀프로젝트</a>
  */
-public class P01001 {
+public class P09466 {
+    static int n;
+    static int[] studentWantToWorkWith;
+    static boolean[] visited;
+    static boolean[] done;
+
+    static int count;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        System.out.println(Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken()));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            n = Integer.parseInt(br.readLine());
+            studentWantToWorkWith = new int[n + 1];
+            visited = new boolean[n + 1];
+            done = new boolean[n + 1];
+            count = 0;
+
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                studentWantToWorkWith[j] = Integer.parseInt(st.nextToken());
+            }
+
+            for (int j = 1; j <= n; j++) {
+                if (!done[j]) {
+                    dfs(j);
+                }
+            }
+
+            System.out.println(n - count);
+        }
+    }
+
+    static public void dfs(int student) {
+        if (visited[student]) {
+            done[student] = true;
+            count++;
+        } else {
+            visited[student] = true;
+        }
+
+        int next = studentWantToWorkWith[student];
+        if (!done[next]) {
+            dfs(next);
+        }
+
+        visited[student] = false;
+        done[student] = true;
     }
 }
