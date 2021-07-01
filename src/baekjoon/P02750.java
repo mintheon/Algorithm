688861eb043b2f16f2da1dc8b37daa0f67c21a16
mintheon <mintheon@mintheon.com>
2021-07-01 23:46:56 +0900

    [BOJ, 2750] 수 정렬하기

---
+++ src/baekjoon/P02750.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @see <a href="boj.kr/2750"수 정렬하기</a>
+ */
+public class P02750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        List<Integer> list = new ArrayList<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for (int number : list) {
+            sb.append(number + "\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
