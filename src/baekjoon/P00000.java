cc047384a0f2877618d6c51c1c811be2c552bef8
mintheon <mintheon@mintheon.com>
2021-07-03 01:48:25 +0900

    [BOJ, 0] 알고리즘 코드 템플릿

---
+++ src/baekjoon/P00000.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @see <a href="boj.kr/0">템플릿</a>
+ */
+public class P00000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}
