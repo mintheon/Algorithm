9ace8e028f582b0e980ac4fe473186a7c83e0314
mintheon <mintheon@mintheon.com>
2021-07-03 00:41:33 +0900

    [BOJ, 2447] 별찍기10 (재시도요망)

--- src/baekjoon/P02447.java
+++ src/baekjoon/P02447.java
@@ -3,16 +3,58 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 /**
- * @see <a href="boj.kr/1001">A-B</a>
+ * @see <a href="boj.kr/2447">별찍기10</a>
  */
-public class P01001 {
+public class P02447 {
+    static String[][] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        System.out.println(Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken()));
+        int n = Integer.parseInt(br.readLine());
+        arr = new String[n][n];
+
+        print(0, 0, n, false);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void print(int x, int y, int n, boolean isBlank) {
+        if (isBlank) {
+            for (int i = x; i < x + n; i++) {
+                for (int j = y; j < y + n; j++) {
+                    arr[i][j] = " ";
+                }
+            }
+            return;
+        }
+
+        if (n == 1) {
+            arr[x][y] = "*";
+            return;
+        }
+
+        int size = n / 3;
+        int count = 0;
+        for (int i = x; i < x + n; i += size) {
+            for (int j = y; j < y + n; j += size) {
+                count++;
+                if (count == 5) {
+                    print(i, j, size, true);
+                } else {
+                    print(i, j, size, false);
+                }
+            }
+        }
     }
 }
