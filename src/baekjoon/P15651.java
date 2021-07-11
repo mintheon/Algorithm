5b64d23cbed905f7ce5fb1355484c75e51375bbd
mintheon <mintheon@mintheon.com>
2021-07-11 16:44:36 +0900

    [BOJ, 15651] N 과 M(3)

---
+++ src/baekjoon/P15651.java
@@ -0,0 +1,48 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @see <a href="boj.kr/15651">N과 M(3)</a>
+ */
+public class P15651 {
+    static int[] numbers;
+
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        numbers = new int[m];
+
+        sb = new StringBuilder();
+
+        int dep = 1;
+        dfs(n, m, dep);
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int n, int m, int dep) {
+        if (dep - 1 == m) {
+            for (int storage : numbers) {
+                sb.append(storage).append(" ");
+            }
+            sb.append("\n");
+
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            numbers[dep - 1] = i;
+            dfs(n, m, dep + 1);
+        }
+    }
+}
