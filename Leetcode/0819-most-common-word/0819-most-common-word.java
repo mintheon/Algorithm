bc4c28c8ad025462c19cc970f0fee550d6e5d466
MinHyun Lee <mintheon@mintheon.com>
2024-10-17 05:41:46 +0900

    Time: 13 ms (77.97%), Space: 43.2 MB (27.14%) - LeetHub

--- Leetcode/0819-most-common-word/0819-most-common-word.java
+++ Leetcode/0819-most-common-word/0819-most-common-word.java
@@ -2,7 +2,7 @@ class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
         Map<String, Integer> counts = new HashMap<>();
 
-        String[] texts = paragraph.toLowerCase().replaceAll("[!?',;.]", " ").split(" ");
+        String[] texts = paragraph.replaceAll("[!?',;.]", " ").toLowerCase().split(" ");
 
         for(String text : texts) {
             if(text == "" || Arrays.asList(banned).indexOf(text) >= 0) {
@@ -11,9 +11,6 @@ class Solution {
             counts.put(text, counts.getOrDefault(text, 0) + 1);
         }
 
-        List<String> keySet = new ArrayList<>(counts.keySet());
-        Collections.sort(keySet, (o1, o2) -> counts.get(o2).compareTo(counts.get(o1)));
-
-        return keySet.get(0);
+        return Collections.max(counts.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }
