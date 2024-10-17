0e233fbcd7311afc63b68e0a8edf0ba9b29a9dbf
MinHyun Lee <mintheon@mintheon.com>
2024-10-17 17:59:21 +0900

    Time: 7 ms (64.73%), Space: 47.8 MB (55.4%) - LeetHub

--- Leetcode/0049-group-anagrams/0049-group-anagrams.java
+++ Leetcode/0049-group-anagrams/0049-group-anagrams.java
@@ -2,17 +2,16 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> anagramsMap = new HashMap<>();
 
-        for(String str : strs) {
+        for (String str : strs) {
             char[] charArr = str.toCharArray();
             Arrays.sort(charArr);
             String sortedStr = String.valueOf(charArr);
 
-            if(anagramsMap.containsKey(sortedStr)) {
-                anagramsMap.get(sortedStr).add(str);
-            } else {
+            if (!anagramsMap.containsKey(sortedStr)) {
                 anagramsMap.put(sortedStr, new ArrayList<>());
-                anagramsMap.get(sortedStr).add(str);
             }
+
+            anagramsMap.get(sortedStr).add(str);
         }
 
         return new ArrayList<>(anagramsMap.values());
