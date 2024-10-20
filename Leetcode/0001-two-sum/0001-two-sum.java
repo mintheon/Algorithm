767ccd4c83f0143c4c4f0f13e93e97c8be5a30ee
MinHyun Lee <mintheon@mintheon.com>
2024-10-20 15:51:40 +0900

    Time: 44 ms (39.82%), Space: 45 MB (31.54%) - LeetHub

--- Leetcode/0001-two-sum/0001-two-sum.java
+++ Leetcode/0001-two-sum/0001-two-sum.java
@@ -1,34 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[2];
-
-        Map<Integer, List<Integer>> numberMap = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
-            List<Integer> values = numberMap.getOrDefault(nums[i], new LinkedList<>());
-            values.add(i);
-
-            numberMap.put(nums[i], values);
-        }
-
-        for(int key : numberMap.keySet()){
-            List<Integer> pair = numberMap.get(target - key);
-            if(pair == null) {
-                continue;
-            }
-
-            if(key != (target - key)) {
-                answer[0] = numberMap.get(key).getFirst();
-                answer[1] = pair.getFirst();
-
-                return answer;
-            } else {
-                answer[0] = numberMap.get(key).getFirst();
-                answer[1] = numberMap.get(key).getLast();
-
-                return answer;
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
             }
         }
 
-        return answer;
+        return null;
     }
 }
