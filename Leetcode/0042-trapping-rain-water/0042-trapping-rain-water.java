12fe49621dd93d6eaf00f5970fb28e001072c6d7
MinHyun Lee <mintheon@mintheon.com>
2024-10-23 00:25:24 +0900

    Time: 1 ms (67.47%), Space: 46.2 MB (47.8%) - LeetHub

---
+++ Leetcode/0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int trap(int[] height) {
+        int volume = 0;
+        int left = 0;
+        int right = height.length - 1;
+        
+        int leftMax = height[left];
+        int rightMax = height[right];
+
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+
+            if(leftMax <= rightMax) {
+                volume += leftMax - height[left];
+                left++;
+            } else {
+                volume += rightMax - height[right];
+                right--;
+            }
+        }
+
+        return volume;
+    }
+}
