1a264e9a0a3cf824f5f2f0849a65cf072a3eea9f
MinHyun Lee <mintheon@mintheon.com>
2024-10-24 00:19:43 +0900

    Time: 4 ms (8.3%), Space: 45.4 MB (97.66%) - LeetHub

--- Leetcode/0042-trapping-rain-water/0042-trapping-rain-water.java
+++ Leetcode/0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,28 @@
 class Solution {
     public int trap(int[] height) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
         int volume = 0;
-        int left = 0;
-        int right = height.length - 1;
-        
-        int leftMax = height[left];
-        int rightMax = height[right];
 
-        while (left < right) {
-            leftMax = Math.max(leftMax, height[left]);
-            rightMax = Math.max(rightMax, height[right]);
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                Integer top = stack.pop();
 
-            if(leftMax <= rightMax) {
-                volume += leftMax - height[left];
-                left++;
-            } else {
-                volume += rightMax - height[right];
-                right--;
+                if(stack.isEmpty()) {
+                    break;
+                }
+
+                // 3 - 1 - 1
+                int distance = i - stack.peek() - 1;
+
+                // 2 or 1 중에 작은거 -> 1
+                // 1 - 0 = 1
+                int waters = Math.min(height[i], height[stack.peek()]) - height[top];
+
+                volume += distance * waters;
             }
+
+            stack.push(i);
         }
 
         return volume;
