4e6b2320d35870ea074cb9fe3d39c84496e22f4c
MinHyun Lee <mintheon@mintheon.com>
2024-12-29 01:52:59 +0900

    Time: 2 ms (89.37%), Space: 55.6 MB (45.63%) - LeetHub

--- Leetcode/0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ Leetcode/0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,24 +1,23 @@
 class Solution {
+    /**
+    공간복잡도: O(n)
+    시간복잡도: O(n)
+     */
     public int[] productExceptSelf(int[] nums) {
         int[] answer = new int[nums.length];
-        
-        int[] prefix = new int[nums.length];
-        int[] postfix = new int[nums.length];
 
-        prefix[0] = 1;
+        answer[0] = 1;
         for(int i = 1; i < nums.length; i++) {
-            prefix[i] = prefix[i - 1] * nums[i - 1];
+            answer[i] = answer[i - 1] * nums[i - 1];
         }
 
-        postfix[nums.length - 1] = 1;
-        for(int i = nums.length - 2; i >= 0; i--) {
-            postfix[i] = postfix[i + 1] * nums[i + 1];
-        }
+        int value = 1;
+        for(int i = nums.length - 1; i >= 0; i--) {
 
-        for(int i = 0; i < nums.length; i++) {
-            answer[i] = prefix[i] * postfix[i];
+            answer[i] = answer[i] * value;
+            value *= nums[i];
         }
-
+        
         return answer;
     }
 }
