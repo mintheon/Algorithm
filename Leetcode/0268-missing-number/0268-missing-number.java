2a4cd34030181b8f48459094538f314bd0d5a44f
MinHyun Lee <mintheon@mintheon.com>
2024-12-29 22:36:11 +0900

    Time: 0 ms (100%), Space: 45.3 MB (45.86%) - LeetHub

--- Leetcode/0268-missing-number/0268-missing-number.java
+++ Leetcode/0268-missing-number/0268-missing-number.java
@@ -1,14 +1,17 @@
 class Solution {
+    /**
+    시간복잡도: O(n)
+    공간복잡도: O(1)
+     */
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
 
-        int i;
-        for(i = 0; i < nums.length; i++) {
-            if(nums[i] != i) {
-                return i;
-            }
+        int arraySum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            arraySum += nums[i];
         }
 
-        return i;
+        return expectedSum - arraySum;
     }
 }
