b355cd6251ded657cd595c61a4284e0e932f5f39
MinHyun Lee <mintheon@mintheon.com>
2025-01-05 04:21:15 +0900

    Time: 1 ms (99.81%), Space: 62 MB (25.63%) - LeetHub

--- Leetcode/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ Leetcode/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,13 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buyPrice = prices[0];
-        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        int maxPrice = 0;
+        int price = 0;
 
-        for(int price : prices) {
-            buyPrice = Math.min(buyPrice, price);
-            maxProfit = Math.max(maxProfit, price - buyPrice);
+        for(int i = 0; i < prices.length; i++) {
+            maxPrice = Math.max(maxPrice, prices[i]);
+
+            if(minPrice > prices[i]) {
+                minPrice = prices[i];
+                maxPrice = prices[i];
+            }
+
+            price = Math.max(price, maxPrice - minPrice);
         }
 
-        return maxProfit;
+        return price;
     }
 }
