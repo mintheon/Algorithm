667f9a3aef9efd0a3e4ac4b4549924f013854f31
MinHyun Lee <mintheon@mintheon.com>
2025-02-22 21:14:07 +0900

    Time: 0 ms (100%), Space: 42.6 MB (44.97%) - LeetHub

--- Leetcode/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ Leetcode/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,23 +10,20 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode answer = new ListNode(-1);
-        ListNode node = answer;
-
-        while(list1 != null && list2 != null) {
-            if(list1.val < list2.val) {
-                node.next = list1;
-                list1 = list1.next;
-            } else {
-                node.next = list2;
-                list2 = list2.next;
-            }
-
-            node = node.next;
+        if(list1 == null) {
+            return list2;
         }
 
-        node.next = list1 != null ? list1 : list2;
+        if(list2 == null) {
+            return list1;
+        }
 
-        return answer.next;
+        if(list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
     }
 }
