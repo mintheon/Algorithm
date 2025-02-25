15827f96031a8b401725117dcbfb837dad50e499
MinHyun Lee <mintheon@mintheon.com>
2025-02-26 01:47:54 +0900

    Time: 3 ms (99.78%), Space: 68.5 MB (42.61%) - LeetHub

--- Leetcode/0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ Leetcode/0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,24 +8,37 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ // 시간복잡도: O(n)
+ // 공간복잡도: O(n)
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Deque<Integer> deque = new LinkedList<>();
+        ListNode slow = head;
+        ListNode fast = head;
 
-        ListNode cur = head;
-        while(cur != null) {
-            deque.addFirst(cur.val);
-            cur = cur.next;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        while(!deque.isEmpty()) {
-            if(deque.size() == 1) {
-                return true;
-            }
+        if(fast != null) {
+            slow = slow.next;
+        }
 
-            if(deque.pollFirst() != deque.pollLast()) {
+        ListNode node = null;
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = node;
+            node = slow;
+            slow = next;
+        }
+
+        while(node != null) {
+            if(node.val != head.val) {
                 return false;
             }
+
+            node = node.next;
+            head = head.next;
         }
 
         return true;
