84835a9096c9fa7a897896e0d8ab98be09ee21a4
MinHyun Lee <mintheon@mintheon.com>
2025-03-09 18:14:16 +0900

    Time: 0 ms (100%), Space: 42 MB (8.8%) - LeetHub

---
+++ Leetcode/0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,32 @@
+class MyStack {
+    List<Integer> list;
+
+    public MyStack() {
+        list = new ArrayList<>();
+    }
+    
+    public void push(int x) {
+        list.add(x);
+    }
+    
+    public int pop() {
+        return list.removeLast();
+    }
+    
+    public int top() {
+        return list.getLast();
+    }
+    
+    public boolean empty() {
+        return list.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
