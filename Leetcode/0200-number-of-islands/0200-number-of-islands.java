e1f5f8a5d09890b309f3fde2f6505ea634147afb
MinHyun Lee <mintheon@mintheon.com>
2025-03-14 00:49:09 +0900

    Time: 11 ms (9.15%), Space: 52.3 MB (20.01%) - LeetHub

--- Leetcode/0200-number-of-islands/0200-number-of-islands.java
+++ Leetcode/0200-number-of-islands/0200-number-of-islands.java
@@ -1,50 +1,52 @@
-//시간복잡도: O(m * n)
-//공간복잡도: O(m * n)
 class Solution {
-    private final char LAND = '1';
-    private final char SEA = '0';
-    private final List<int[]> DIRECTIONS = Arrays.asList(
-            new int[] { -1, 0 },
-            new int[] { 0, 1 },
-            new int[] { 1, 0 },
-            new int[] { 0, -1 });
+    int length;
+    int width;
+    List<int[]> move = new LinkedList<>(Arrays.asList(new int[]{1, 0}, new int[]{-1, 0}, new int[]{0, -1}, new int[]{0, 1}));
+    boolean[][] visited;
 
     public int numIslands(char[][] grid) {
-        int count = 0;
-        boolean visited[][] = new boolean[grid.length][grid[0].length];
+        length = grid.length;
+        width = grid[0].length;
+        visited = new boolean[length][width];
 
-        for(int row = 0; row < grid.length; row++) {
-            for(int col = 0; col < grid[0].length; col++) {
-                if(grid[row][col] == LAND && !visited[row][col]) {
-                    count++;
-                    Queue<int[]> queue = new LinkedList<>();
-
-                    queue.add(new int[] {col, row});
-                    visited[row][col] = true;
-
-                    while(!queue.isEmpty()) {
-                        int[] cur = queue.poll();
-
-                        for(int[] directions : DIRECTIONS) {
-                            int x = cur[0] + directions[0];
-                            int y = cur[1] + directions[1];
-
-                            if(x < 0 || y < 0 || x >= grid[0].length || y >= grid.length || visited[y][x]) {
-                                continue;
-                            }
-
-                            if(grid[y][x] != LAND) {
-                                continue;
-                            }
-
-                            visited[y][x] = true;
-                            queue.add(new int[] {x, y});
-                        }
-                    }
+        int landCount = 0;
+        for(int i = 0; i < length; i++) {
+            for(int j = 0; j < width; j++) {
+                if(grid[i][j] == '0' || visited[i][j]) {
+                    continue;
                 }
+
+                visitedIsland(grid, j, i);
+                landCount++;
             }
         }
 
-        return count;
+        return landCount;
+    }
+
+    private void visitedIsland(char[][]grid, int x, int y) {
+        Deque<int[]> queue = new ArrayDeque<>();
+
+        queue.add(new int[]{x, y});
+        visited[y][x] = true;
+        
+        while(!queue.isEmpty()) {
+            int[] p = queue.poll();
+
+            for(int[] point : move) {
+                int moveX = p[0] + point[0];
+                int moveY = p[1] + point[1];
+
+                if(moveX < 0 || moveY < 0 || moveX >= width || moveY >= length) {
+                    continue;
+                }
+                if(grid[moveY][moveX] == '0' || visited[moveY][moveX]) {
+                    continue;
+                }
+
+                queue.add(new int[]{moveX, moveY});
+                visited[moveY][moveX] = true;
+            }
+        }
     }
 }
