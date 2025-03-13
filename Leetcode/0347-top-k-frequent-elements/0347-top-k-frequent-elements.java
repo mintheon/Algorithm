09556f0af27438153a7c5c16d0bdc82a41a17229
MinHyun Lee <mintheon@mintheon.com>
2025-03-13 23:07:34 +0900

    Time: 14 ms (59.09%), Space: 48.9 MB (43.32%) - LeetHub

--- Leetcode/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ Leetcode/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,17 +1,25 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int[] answer = new int[k];
-        Map<Integer, Integer> frequent = new HashMap<>();
+        // 1,3 / 2,2 / 3,1
+        // int[] -> o2 - o1
 
-        for(int num: nums) {
-            frequent.put(num, frequent.getOrDefault(num, 1) + 1);
+        //시간복잡도: nlogn
+        //공간복잡도: n
+        PriorityQueue<int[]> top = new PriorityQueue<>((o1, o2) -> o2[1] - o1[1]);
+        Map<Integer, Integer> counter = new HashMap<>();
+
+        for(int num : nums) {
+            int count = counter.getOrDefault(num, 0);
+            counter.put(num, count + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue().compareTo(a.getValue()));
-        pq.addAll(frequent.entrySet());
+        for(int key : counter.keySet()) {
+            top.offer(new int[]{key, counter.get(key)});
+        }
 
+        int[] answer = new int[k];
         for(int i = 0; i < k; i++) {
-            answer[i] = pq.poll().getKey();
+            answer[i] = top.poll()[0];
         }
 
         return answer;
