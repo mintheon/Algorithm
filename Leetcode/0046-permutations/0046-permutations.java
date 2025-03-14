7ebb093571f54a3b7fa3aac4d2f08c47b80bc81e
MinHyun Lee <mintheon@mintheon.com>
2025-03-15 00:42:53 +0900

    Time: 2 ms (39.97%), Space: 44.7 MB (31.36%) - LeetHub

---
+++ Leetcode/0046-permutations/0046-permutations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+
+        dfs(answer, new HashSet<>(), nums, new ArrayList<>());
+
+        return answer;
+    }
+
+    public void dfs(List<List<Integer>> answer, Set<Integer> used, int[] nums, List<Integer> permuteNum) {
+        if(permuteNum.size() == nums.length) {
+            answer.add(new ArrayList<>(permuteNum));
+            return;
+        }
+
+        for(int num : nums) {
+            if(!used.contains(num)) {
+                permuteNum.add(num);
+                used.add(num);
+                dfs(answer, used, nums, permuteNum);
+                permuteNum.remove(permuteNum.size() - 1);
+                used.remove(num);
+            }
+        }
+    }
+}
