d8d0bf799193c9d887c07d4785bcde9fdee9c524
MinHyun Lee <mintheon@mintheon.com>
2025-03-15 14:35:22 +0900

    Time: 2 ms (40.01%), Space: 44.4 MB (81.59%) - LeetHub

--- Leetcode/0046-permutations/0046-permutations.java
+++ Leetcode/0046-permutations/0046-permutations.java
@@ -1,28 +1,32 @@
-//시간 복잡도: O(n! * n)
-//공간 복잡도: O(n * n!)
+//1 2 4
+//124
+//1 2 + 4
+//1 + 2
+//1
+
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
 
-        dfs(answer, new HashSet<>(), nums, new ArrayList<>());
+        dfs(answer, nums, new ArrayList<>());
 
         return answer;
     }
 
-    public void dfs(List<List<Integer>> answer, Set<Integer> used, int[] nums, List<Integer> permuteNum) {
-        if(permuteNum.size() == nums.length) {
-            answer.add(new ArrayList<>(permuteNum));
+    public void dfs(List<List<Integer>> answer, int[] nums, List<Integer> permuteNums) {
+        if(permuteNums.size() == nums.length) {
+            answer.add(new ArrayList<>(permuteNums));
             return;
         }
 
-        for(int num : nums) {
-            if(!used.contains(num)) {
-                permuteNum.add(num);
-                used.add(num);
-                dfs(answer, used, nums, permuteNum);
-                permuteNum.remove(permuteNum.size() - 1);
-                used.remove(num);
+        for(Integer num : nums) {
+            if(permuteNums.contains(num)) {
+                continue;
             }
+
+            permuteNums.add(num);
+            dfs(answer, nums, permuteNums);
+            permuteNums.remove(num);
         }
     }
 }
