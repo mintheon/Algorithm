46cad88482f4df5d7b70b022984341c83b564d90
MinHyun Lee <mintheon@mintheon.com>
2025-03-16 22:54:33 +0900

    Time: 4 ms (59.55%), Space: 44.5 MB (97.43%) - LeetHub

--- Leetcode/0001-two-sum/0001-two-sum.java
+++ Leetcode/0001-two-sum/0001-two-sum.java
@@ -1,23 +1,25 @@
+//시간복잡도: O(n)
+//공간복잡도: O(1)
 class Solution {
-    /**
-    공간복잡도: O(n)
-    시간복잡도: O(n)
-     */
-
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numMap = new HashMap<>();
+        Map<Integer, Integer> positions = new HashMap<>();
 
         for(int i = 0; i < nums.length; i++) {
-            numMap.put(nums[i], i);
+            positions.put(nums[i], i);
         }
 
         for(int i = 0; i < nums.length; i++) {
-            int pairNum = target - nums[i];
-            if(numMap.containsKey(pairNum) && numMap.get(pairNum) != i) {
-                return new int[]{i, numMap.get(target - nums[i])};
+            if(!positions.containsKey(target - nums[i])) {
+                continue;
+            }
+
+            int position = positions.get(target - nums[i]);
+            
+            if(position != i) {
+                return new int[]{i, position};
             }
         }
 
-        return new int[2];
+        return new int[]{};
     }
 }
