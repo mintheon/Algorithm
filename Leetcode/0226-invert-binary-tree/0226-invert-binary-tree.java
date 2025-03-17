181fd798888356f269c5ba62a1a99d9b745d7a63
MinHyun Lee <mintheon@mintheon.com>
2025-03-17 22:22:03 +0900

    Time: 0 ms (100%), Space: 40.8 MB (98.07%) - LeetHub

--- Leetcode/0226-invert-binary-tree/0226-invert-binary-tree.java
+++ Leetcode/0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -13,37 +13,26 @@
  *     }
  * }
  */
- //시간복잡도: O(n)
- //공간복잡도: O(n)
+
+//시간복잡도: O(n)
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null) {
-            return root;
-        }
-
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.add(root);
-
-        while(!queue.isEmpty()) {
-            TreeNode cur = queue.poll();
-
-            if(cur == null) {
-                continue;
-            }
-
-            TreeNode temp = cur.left;
-            cur.left = cur.right;
-            cur.right = temp;
-
-            if(cur.left != null) {
-                queue.add(cur.left);
-            }
-
-            if(cur.right != null) {
-                queue.add(cur.right);
-            }
-        }
+        TreeNode cur = root;
+        dfs(cur);
 
         return root;
+    }
+
+    public void dfs(TreeNode cur) {
+        if(cur == null) {
+            return;
+        }
+
+        dfs(cur.left);
+        dfs(cur.right);
+
+        TreeNode temp = cur.left;
+        cur.left = cur.right;
+        cur.right = temp;
     }
 }
