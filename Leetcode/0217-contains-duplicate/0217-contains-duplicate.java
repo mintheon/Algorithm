5076f1802b56b1cf5c24bb05c590bf9370132b2d
MinHyun Lee <mintheon@mintheon.com>
2025-03-30 02:31:22 +0900

    Time: 11 ms (71.34%), Space: 58.1 MB (52.65%) - LeetHub

--- Leetcode/0217-contains-duplicate/0217-contains-duplicate.java
+++ Leetcode/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>(nums.length);
+        Set<Integer> set = new HashSet<>();
 
         for(int num : nums) {
             if(set.contains(num)) {
-                return true;
+               return true; 
             }
 
             set.add(num);
